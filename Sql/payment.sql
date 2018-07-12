ea5bec7377f344ad4fe169dd9a16254c47b6331e
17aim17 <17aim17@gmail.com>
2018-07-12 19:53:00 +0530

    updated sql files

--- Sql/payment.sql
+++ Sql/payment.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 11, 2018 at 08:20 AM
+-- Generation Time: Jul 12, 2018 at 02:21 PM
 -- Server version: 5.1.53
 -- PHP Version: 5.3.4
 
@@ -26,17 +26,19 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `payment` (
+  `Transaction_id` varchar(32) NOT NULL,
   `Transfer_from` varchar(11) NOT NULL,
   `Transfer_to` varchar(11) NOT NULL,
   `Amount` double NOT NULL,
   `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Remarks` varchar(255) NOT NULL
+  `Remarks` varchar(255) NOT NULL,
+  UNIQUE KEY `Transaction_id` (`Transaction_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `payment`
 --
 
-INSERT INTO `payment` (`Transfer_from`, `Transfer_to`, `Amount`, `Date`, `Remarks`) VALUES
-('1234567', '2345678', 28, '2018-07-11 13:18:52', 'First Payment'),
-('1234567', '2345678', 30, '2018-07-11 13:20:05', '');
+INSERT INTO `payment` (`Transaction_id`, `Transfer_from`, `Transfer_to`, `Amount`, `Date`, `Remarks`) VALUES
+('AS20181011531402849', '1234567', '2345678', 333, '2018-07-12 19:10:49', 'unique id 3'),
+('AS20181011531404042', '1234567', '2345678', 25, '2018-07-12 19:30:42', 'unique id 4');
--- Sql/transactions.sql
+++ Sql/transactions.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 11, 2018 at 08:20 AM
+-- Generation Time: Jul 12, 2018 at 02:22 PM
 -- Server version: 5.1.53
 -- PHP Version: 5.3.4
 
@@ -26,23 +26,23 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `transactions` (
-  `Transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Transaction_id` varchar(32) NOT NULL,
   `Account_number` varchar(255) NOT NULL,
   `Amount` double NOT NULL,
   `Date_of_transaction` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Type` varchar(255) NOT NULL,
   `Details` text NOT NULL,
   PRIMARY KEY (`Transaction_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `transactions`
 --
 
 INSERT INTO `transactions` (`Transaction_id`, `Account_number`, `Amount`, `Date_of_transaction`, `Type`, `Details`) VALUES
-(1, '1234567', 26, '2018-07-11 12:44:47', 'Debit', 'First Payment'),
-(2, '2345678', 26, '2018-07-11 12:44:47', 'Credit', 'First Payment'),
-(3, '1234567', 28, '2018-07-11 13:18:52', 'Debit', 'First Payment'),
-(4, '2345678', 28, '2018-07-11 13:18:52', 'Credit', 'First Payment'),
-(5, '1234567', 30, '2018-07-11 13:20:05', 'Debit', ''),
-(6, '2345678', 30, '2018-07-11 13:20:05', 'Credit', '');
+('AS20181011531402152', '1234567', 29, '2018-07-12 18:59:12', 'Credit', 'unique id'),
+('AS20181011531402170', '1234567', 29, '2018-07-12 18:59:30', 'Debit', 'unique id 2'),
+('AS20181011531402849', '1234567', 333, '2018-07-12 19:10:49', 'Debit', 'unique id 3'),
+('AS20181021531402849', '2345678', 333, '2018-07-12 19:10:49', 'Credit', 'unique id 3'),
+('AS20181011531404042', '1234567', 25, '2018-07-12 19:30:42', 'Debit', 'unique id 4'),
+('AS20181021531404042', '2345678', 25, '2018-07-12 19:30:42', 'Credit', 'unique id 4');
